ce132456ca14d5807df273af4dc13679a85d29b6
Sergbab86 <Qazx1324!>
2020-11-05 16:29:06 +0200

    .

--- homework_4.py
+++ homework_4.py
@@ -1,40 +1,30 @@
 
-password = '213343dr56'
-
-
-def validate_password(password):
-    letters_list = []
-    numbers_list = []
-    letters_list = [letters_list.append for i in password if i.isalpha()]
-    numbers_list = [numbers_list.append for i in password if i.isdigit()]
-    if not password.isalnum():
-        print('Строка содержит запрещенные символы')
-    elif len(letters_list) % 2 != 0:
-        print('Содержит нечетное количество букв')
-    elif len(numbers_list) % 2 == 0:
-        print('Содержит четное количество цифр')
+password = 'g786^&r'
 
 
 def _validate_symbols(password):
-    if not password.isalnum():
-        return False
-    return True
+    return password.isalnum()
 
 
 def _validate_letters_even(password):
-    letters_list = []
-    letters_list = [letters_list.append for i in password if i.isalpha()]
-    if len(letters_list) % 2 != 0:
-        return False
-    return True
+    letters_list = [i for i in password if i.isalpha()]
+    return not len(letters_list) % 2
 
 
 def _validate_numbers_odd(password):
-    numbers_list = []
-    numbers_list = [numbers_list.append for i in password if i.isdigit()]
-    if len(numbers_list) % 2 == 0:
+    numbers_list = [i for i in password if i.isdigit()]
+    return len(numbers_list) % 2 == 0
+
+
+def validate_password(password):
+    if _validate_symbols:
+        print('Строка содержит запрещенные символы')
         return False
-    return True
+    elif _validate_letters_even:
+        print('Содержит нечетное количество букв')
+    elif not _validate_numbers_odd:
+        print('Содержит четное количество цифр')
+
 
 
 print(validate_password(password))
