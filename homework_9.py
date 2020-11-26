50508acc22117ca9f6eefa2751561d187e8d0626
Sergbab86 <Qazx1324!>
2020-11-26 17:16:07 +0200

    Добрый день. Не успел сделать домашку, сдам сейчас чтобы была кнопка пересдать. Оценка не нужна)

---
+++ homework_9.py
@@ -0,0 +1,13 @@
+
+import uuid
+from user import User
+from order import Order
+from customer import Customer
+
+
+class Review(Customer):
+    def __init__(self, text, mark, status = 'Moderation')
+        self.text = text
+        self.mark = mark
+        self.status = status
+    def
